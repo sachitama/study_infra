6106e2c36f92f4a851749977e1b90a7c78a24d77
sachitama <sachi.github@gmail.com>
2022-11-03 23:25:38 +0900

    terraform学習記録

---
+++ main.tf
@@ -0,0 +1,47 @@
+# -----------------------------
+# terraform configuration
+# -----------------------------
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "tastylog-tfstate-bucket-sachi"
+    key     = "tastylog-dev.tfstate"
+    region  = "ap-northeast-1"
+    profile = "terraform"
+  }
+}
+
+# -----------------------------
+# provider
+# -----------------------------
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+provider "aws" {
+  alias   = "virginia"
+  profile = "terraform"
+  region  = "us-east-1"
+}
+
+# -----------------------------
+# variables
+# -----------------------------
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "domain" {
+  type = string
+}
