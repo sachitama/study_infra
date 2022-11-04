72c0eadc673d628ad1db061b6cc86f4c69b36b16
sachitama <sachi.github@gmail.com>
2022-11-04 13:33:56 +0900

    fix

--- cloudfront.tf
+++ cloudfront.tf
@@ -76,7 +76,7 @@ resource "aws_cloudfront_distribution" "cf" {
   aliases = ["dev.${var.domain}"]
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.virginia_cert.arn
+    acm_certificate_arn      = aws_acm_certificate.tokyo_cert.arn
     minimum_protocol_version = "TLSv1.2_2019"
     ssl_support_method       = "sni-only"
   }
